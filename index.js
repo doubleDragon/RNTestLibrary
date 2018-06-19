313cc17d9ae2968c819c7ece12b31eee324d4b89
doubleDragon <aiai373824745@gmail.com>
2018-06-19 20:49:12 +0800

    Add some js file

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 /* eslint-disable global-require */
 module.exports = {
     get AdMobBanner() {
-        return require('./RNGADBannerView').default;
+        return require('./RNAdMobBanner').default;
     },
     get AdMobInterstitial() {
         return require('./RNAdMobInterstitial').default;
     },
     get PublisherBanner() {
-        return require('./RNDFPBannerView').default;
+        return require('./RNPublisherBanner').default;
     },
     get AdMobRewarded() {
         return require('./RNAdMobRewarded').default;
